d83ed9c6a27303f995fd6dddf05d6a0d02a8c3a4
Cloud <cloudcry@gmail.com>
2018-10-28 19:32:16 +0800

    add bool tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -8,6 +8,34 @@ spec :: Spec
 spec = do 
   describe "Parser JSON" $ do 
     describe "parse string" $ do 
-      context "given \"abc cde\", zzz" $ do 
-        it "can parse \"abc cde\"" $ do
-          parse jsonString "(undfined)" "\"abc cde\"zzz" `shouldBe` return "abc cde"
+      it "parse \"abc cde\", zzz" $ do
+        parse jString "(undfined)" "\"abc cde\", zzz" `shouldBe` return "abc cde"
+      it "parse \r, zzz" $ do
+        parse jString "(undfined)" "\"\r\", zzz" `shouldBe` return "\r"
+      it "parse \t, zzz" $ do
+        parse jString "(undfined)" "\"\t\", zzz" `shouldBe` return "\t"
+      it "parse \n, zzz" $ do
+        parse jString "(undfined)" "\"\n\", zzz" `shouldBe` return "\n"
+    describe "parse bool" $ do
+      context "parse bool to True value" $ do
+        it "parse true" $ do 
+          parse jBool "(undfined)" "true" == return True
+        it "parse true," $ do 
+          parse jBool "(undfined)" "true," == return True
+        it "can't parse trueabc" $ do 
+          parse jBool "(undfined)" "trueabc" /= return True
+        it "parse true " $ do 
+          parse jBool "(undfined)" "true " == return True
+        it "can't parse True " $ do 
+          parse jBool "(undfined)" "True " /= return True
+      context "parse bool to False value" $ do
+        it "parse false" $ do
+          parse jBool "(undfined)" "false" == return False
+        it "parse false," $ do
+          parse jBool "(undfined)" "false," == return False
+        it "can't parse falseabc" $ do
+          parse jBool "(undfined)" "falseabc" /= return False
+        it "parse false " $ do
+          parse jBool "(undfined)" "false " == return False
+        it "can't parse False " $ do
+          parse jBool "(undfined)" "False " /= return False
